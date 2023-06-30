5dc6597fe9c2a254296c4ad342c553e57e6b1b22
0-knight <zeroknight.eth@gmail.com>
2023-06-30 14:37:32 +0900

    done with implementing schnorr sequencer scheme in schnorr.rs

--- src/schnorr.rs
+++ src/schnorr.rs
@@ -26,14 +26,15 @@ pub fn nk_generator() -> PointAffine {
 
 
 // == Key pair == //
-pub struct SecretKey(Base);
+#[derive(Clone)]
+pub struct SecretKey(Scalar);
 impl SecretKey {
-    pub fn inner(&self) -> Base {
+    pub fn inner(&self) -> Scalar {
         self.0                      // [WIP] ??
     }
 
     pub fn random(rng : &mut (impl CryptoRng + RngCore)) -> Self {
-        Self(Base::random(rng))
+        Self(Scalar::random(rng))
     }
 }
 
@@ -46,7 +47,7 @@ impl PublicKey {
     }
 
     pub fn from_secret( s: SecretKey) -> Self {
-        let p = nk_generator() * mod_r_p(s.inner());
+        let p = nk_generator() * s.inner();
         Self(p)
     }
 }
@@ -65,15 +66,26 @@ impl Signature {
     }
 }
 
+pub struct SharedValue {
+    hash_pubs : Base,   // hash value of the public key sets
+    challenge : Base,   // challenge e : e = H (R | X | m | index)
+}
+
+
 // Trait for secret keys that implements a signature creation
 pub trait SchnorrSecret {
     // Sign a given message, using 'rng' as source of randomness
     fn sign(&self, rng: &mut (impl CryptoRng + RngCore), message: &[u8;32]) -> Signature;
+    fn sign_ext(&self, rng: &mut (impl CryptoRng + RngCore), message: &[u8;32], 
+                nonce : Scalar, weight_factor : Base, challenge : Base, ) -> Signature;
+    
 }
 
 pub trait SchnorrPublic {
     // Verify a given message is valid given a signature
     fn verify(&self, message: &[u8;32], signature: &Signature) -> bool;
+    fn verify_ext(&self, message: &[u8;32], signature: &Signature, weight_factor : Base, challenge: Base) -> bool;
+    
 }
 
 // Convert from Base to Scalar (aka $x \pmod{r_\mathbb{P}}$)
@@ -92,10 +104,23 @@ impl SchnorrSecret for SecretKey {
 
         // generate a challenge e
         let challenge = poseidon_hash(vec![commit.x, commit.y, message_base]);
-        let response = mask + mod_r_p(challenge) * mod_r_p(self.inner());
+        let response = mask + mod_r_p(challenge) * self.inner();
 
         Signature { commit, response }
     }
+
+    fn sign_ext(&self, rng: &mut (impl CryptoRng + RngCore), message: &[u8;32], 
+                nonce : Scalar, weight_factor : Base, challenge : Base, ) -> Signature {
+        let commit = nk_generator() * nonce;
+
+        // s_a = r_a + k_a * w_a * e
+        let response = nonce + self.0 * mod_r_p(weight_factor) * mod_r_p(challenge);
+
+        Signature { commit, response }
+    }
+
+
+
 }
 
 impl SchnorrPublic for PublicKey {
@@ -106,6 +131,13 @@ impl SchnorrPublic for PublicKey {
         let challenge = poseidon_hash(vec![signature.commit.x, signature.commit.y, message_base ]);
         nk_generator() * signature.response - self.inner() * mod_r_p(challenge) == signature.commit
     }
+    // public_nonce: Base is replaced by commit : nonce * G
+    fn verify_ext(&self, message: &[u8;32], signature: &Signature, weight_factor : Base, challenge: Base) -> bool {
+
+        //sG == R + e(wkG) = R + ePw
+        nk_generator() * signature.response - (self.inner() * mod_r_p(weight_factor) * mod_r_p(challenge)) == signature.commit 
+
+    }
 }
 
 #[test]
@@ -115,4 +147,54 @@ fn test_schnorr_sig() {
     let signature = secret.sign(&mut OsRng, message);
     let public = PublicKey::from_secret(secret);
     assert!(public.verify(message, &signature));
+}
+
+#[test]
+fn test_schnorr_sig_ext() {
+    // User
+    let k_u = SecretKey::random(&mut OsRng);
+    let P_u = PublicKey::from_secret(k_u.clone());
+    let r_u = SecretKey::random(&mut OsRng).inner();        // random nonce : Scalar
+    let R_u = PublicKey::from_secret(SecretKey((r_u))).inner(); // public Nonce : Point
+
+    // Sequencer
+    let k_s = SecretKey::random(&mut OsRng);
+    let P_s = PublicKey::from_secret(k_s.clone());
+    let r_s = SecretKey::random(&mut OsRng).inner();        // random nonce : Scalar
+    let R_s = PublicKey::from_secret(SecretKey((r_u))).inner(); // public Nonce : Point
+
+    let R = R_u + R_s;
+    let l = poseidon_hash(vec![P_u.inner().x , P_u.inner().y, P_s.inner().x, P_s.inner().y]); // Base Field
+    let w_u = poseidon_hash(vec![l , P_u.inner().x, P_u.inner().y ]);
+    let w_s = poseidon_hash(vec![l, P_s.inner().x, P_s.inner().y]);
+    
+    let message: &[u8;32] = &[1;32];
+    let message_base = Base::from_bytes(message.into()).unwrap();
+
+    let X = P_u.inner() * mod_r_p(w_u) + P_s.inner() * mod_r_p(w_s);     // w_u * P_u doesn't work!!!
+    let e = poseidon_hash(vec![R.x, R.y, X.x, X.y, message_base, Base::from(0)]);
+
+/*
+fn sign_ext(&self, rng: &mut (impl CryptoRng + RngCore), message: &[u8;32], 
+                nonce : Scalar, weight_factor : Base, challenge : Base, ) -> Signature {
+ */
+    let signature_u = k_u.sign_ext(&mut OsRng, message, r_u, w_u, e);
+    assert!(P_u.verify_ext(message, &signature_u, w_u, e));
+
+    let signature_s = k_s.sign_ext(&mut OsRng, message, r_s, w_s, e);
+    assert!(P_s.verify_ext(message, &signature_s, w_s, e));
+
+    // full schnorr signature
+    let signature = Signature{
+            response : signature_s.response + signature_u.response,    // Scalar - signature
+            commit : signature_s.commit + signature_u.commit,
+    };
+
+    // The process below should be done in a smart contract..
+    // (s - s_u) * G == (R - R_u) + e (X - w_u * P_u)
+    let left = nk_generator() * (signature.response - signature_u.response);
+    let right = (signature.commit - signature_u.commit) 
+                    + (X - P_u.inner() * mod_r_p(w_u)) * mod_r_p(e);
+    assert_eq!(left, right)
+
 }
